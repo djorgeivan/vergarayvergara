eb33c4103f3c98f60bde72f1b718ac265ec894b5
Jorge Durango <djorge.ivan97@gmail.com>
2017-07-13 21:34:38 -0500

    active admin

---
+++ app/admin/post.rb
@@ -0,0 +1,40 @@
+ActiveAdmin.register Post do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+  permit_params :title, :neighborhood, :area, :price, :details, :phone, :landline, :kind
+
+
+  index do 
+    column :title
+    column :price do |post|
+      number_to_currency post.price
+    end
+    column :created_at
+    actions
+  end
+
+
+  form do |f|
+    f.inputs "Admin Details" do
+      f.input :title
+      f.input :kind, as: :select, :selected => "Casas",  :collection => ["Casas", "Apartamentos", "Lotes", "Fincas", "Bodegas"]
+      f.input :neighborhood
+      f.input :area
+      f.input :price
+      f.input :details
+      f.input :phone
+      f.input :landline
+    end
+    f.actions
+  end
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
