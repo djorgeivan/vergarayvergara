1db972b9dee9f745133084dc0e632857e34435d6
Jorge Durango <djorge.ivan97@gmail.com>
2017-07-25 16:32:45 -0500

    show view

--- app/controllers/inmuebles_controller.rb
+++ app/controllers/inmuebles_controller.rb
@@ -2,4 +2,13 @@ class InmueblesController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    @post = article_id
+  end
+
+  private
+    def article_id
+      Post.find(params[:id])
+    end
 end
