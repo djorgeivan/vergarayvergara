bb334563561eae0701a966fd41df4fe5f1bfdebe
Jorge Durango <djorge.ivan97@gmail.com>
2017-08-21 13:09:14 -0500

    removed dropbox from initializers

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -200,7 +200,6 @@ ActiveAdmin.setup do |config|
   #
   # To load a javascript file:
   #   config.register_javascript 'my_javascript.js'
-  config.register_javascript 'Dropbox.js'
   # == CSV options
   #
   # Set the CSV builder separator
