f34703f0487983ab9e36177854bde9ecd98ba80a
Jorge Durango <djorge.ivan97@gmail.com>
2017-09-10 15:30:41 -0500

    phone number

--- app/helpers/inmuebles_helper.rb
+++ app/helpers/inmuebles_helper.rb
@@ -3,4 +3,12 @@ module InmueblesHelper
     number.to_i
   end
 
+  def makePhone(phone) 
+    
+    format = "(xxx) xxx-xxxx"
+    for i in (0..10) 
+      format = format.gsub("x", phone[i])
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170910190039) do
+ActiveRecord::Schema.define(version: 20170910200229) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 20170910190039) do
     t.float    "area"
     t.float    "price"
     t.text     "details"
-    t.integer  "phone"
+    t.string   "phone"
     t.string   "landline",     limit: 10
     t.string   "type"
     t.datetime "created_at",              null: false
